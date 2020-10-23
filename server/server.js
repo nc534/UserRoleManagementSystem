63ca1fff9150f5d98802b21cf5626c8b8bf2ee94
Noel <noel.cheng@rutgers.edu>
2020-10-22 20:29:16 -0400

    add connection to database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const app = express();
+const mysql = require('@mysql/xdevapi');
+
+require("dotenv").config();
+
+//connect to MySQL db
+mysql.getSession({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    schema: process.env.DB,
+    port: process.env.PORT
+}).then(() => {
+    console.log("Connected to database...")
+}).catch((err) => {
+    if(err) {
+        console.error(err.message);
+    }
+})
 
 //listen to the server and send a message to ensure server started
 app.listen(3000, () => {
